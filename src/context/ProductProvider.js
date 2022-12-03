0b77b5567407183058c965717429857f1fb19276
neyaznafiz <niiyazobug@gmail.com>
2022-12-03 21:56:38 +0600

    get product in home using useProducts hook and display all products in home

--- src/context/ProductProvider.js
+++ src/context/ProductProvider.js
@@ -1,24 +1,40 @@
-import React, { createContext, useContext, useEffect, useReducer, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import axios from "axios";
-import { initialState, productReducer } from "../state/productState/ProductReducer";
+import {
+  initialState,
+  productReducer,
+} from "../state/productState/ProductReducer";
+import { actionTypes } from "../state/productState/actionTypes";
 
 const CONTEXT = createContext();
 
 function ProductProvider({ children }) {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
   // useReducer
-  const [state, dispatch] = useReducer(productReducer, initialState)
-
+  const [state, dispatch] = useReducer(productReducer, initialState);
+  console.log(state);
   useEffect(() => {
-    axios.get("http://localhost:5000/products").then((res) => {
-      const data = res;
-      setData(data.data);
-    });
+    dispatch({ type: actionTypes.FETCHING_START });
+    axios
+      .get("http://localhost:5000/products")
+      .then((res) => {
+        const data = res;
+        // setData(data.data);
+        dispatch({ type: actionTypes.FETCHING_SUCCESS, payload: data.data.data });
+      })
+      .catch(dispatch({ type: actionTypes.FETCHING_ERROR }));
   }, []);
 
   const value = {
-    data,
+    state,
+    dispatch,
   };
 
   return <CONTEXT.Provider value={value}>{children}</CONTEXT.Provider>;
