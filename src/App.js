a47f58ce36083225dc4deb8e0c5f1604d1dd0350
neyaznafiz <niiyazobug@gmail.com>
2022-12-03 19:16:15 +0600

    product provider created and context create here and also create a useProducts hook here

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
-import { createContext, useEffect, useState } from "react";
+import React from "react";
 import { RouterProvider } from "react-router-dom";
+import ProductProvider from "./context/ProductProvider";
 import routes from "./routes/routes";
-import axios from 'axios'
-
-const CONTEXT = createContext();
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/products')
-      .then(res => {
-        const data = res
-        setData(data.data)
-        console.log(data.data)
-    })
-  }, [])
-
-  const value = {
-    data,
-  };
   return (
     <div>
-      <CONTEXT.Provider value={value}>
+      <ProductProvider>
         <RouterProvider router={routes} />
-      </CONTEXT.Provider>
+      </ProductProvider>
     </div>
   );
 }
---
+++ src/context/ProductProvider.js
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const CONTEXT = createContext();
+
+function ProductProvider({ children }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/products").then((res) => {
+      const data = res;
+      setData(data.data);
+      console.log(data.data);
+    });
+  }, []);
+
+  const value = {
+    data,
+  };
+
+  return
+  <CONTEXT.Provider value={value}>{children}</CONTEXT.Provider>;
+}
+
+export const useProducts = () => {
+  const context = useContext(CONTEXT);
+  return context;
+};
+
+export default ProductProvider;
