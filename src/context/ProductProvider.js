fa336e4b28d9dc19f4e8d7784cac9bff63116a5c
neyaznafiz <niiyazobug@gmail.com>
2022-12-03 21:38:34 +0600

    initialstate, produce reducer and actionTypes impliment for reducer

--- src/context/ProductProvider.js
+++ src/context/ProductProvider.js
@@ -1,10 +1,14 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useReducer, useState } from "react";
 import axios from "axios";
+import { initialState, productReducer } from "../state/productState/ProductReducer";
 
 const CONTEXT = createContext();
 
 function ProductProvider({ children }) {
   const [data, setData] = useState([]);
+
+  // useReducer
+  const [state, dispatch] = useReducer(productReducer, initialState)
 
   useEffect(() => {
     axios.get("http://localhost:5000/products").then((res) => {
@@ -20,6 +24,7 @@ function ProductProvider({ children }) {
   return <CONTEXT.Provider value={value}>{children}</CONTEXT.Provider>;
 }
 
+// useProduct hook
 export const useProducts = () => {
   const context = useContext(CONTEXT);
   return context;
