cdc22871e2b5c644b594a668734e18ebddf5cf0f
neyaznafiz <niiyazobug@gmail.com>
2022-12-05 16:02:52 +0600

    data fetch and show in home, and producetReducer initial setup for redux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  console.log(products);
+
+  useEffect(() => {
+    axios.get("products.json").then((res) => {
+      const data = res;
+      setProducts(data.data);
+    });
+  }, []);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      this is home
+      {products.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
     </div>
   );
 };
