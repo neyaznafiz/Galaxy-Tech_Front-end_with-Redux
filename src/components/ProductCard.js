e1010b174fce1d489ca1038c0c1668983f2b68a1
neyaznafiz <niiyazobug@gmail.com>
2022-12-05 18:59:02 +0600

    add to cart function create in actionCreateo file and called it in dispatch in productCard component

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/actionCreators/productActions";
 import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
 
 const ProductCard = ({ product }) => {
@@ -29,7 +30,7 @@ const ProductCard = ({ product }) => {
       </div>
       <div className="flex gap-2 mt-5">
         <button
-          onClick={() => dispatch({ type: ADD_TO_CART, payload: product })}
+          onClick={() => dispatch(addToCart(product))}
           className="bg-[#ccac00] rounded-full py-1 px-2 flex-1 text-white text-bold"
         >
           Add to cart
