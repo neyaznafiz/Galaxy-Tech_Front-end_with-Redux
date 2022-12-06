df50327d826014373c45fff08a85564be6b978df
neyaznafiz <niiyazobug@gmail.com>
2022-12-06 15:00:34 +0600

    clear

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
@@ -13,9 +12,6 @@ const Home = () => {
       setProducts(data.data);
     });
   }, []);
-
-  const state = useSelector((state) => state);
-  // console.log(state);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
