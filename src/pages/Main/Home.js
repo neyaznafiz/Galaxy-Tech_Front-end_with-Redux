54c116ce54b6b1ec8e542595a8580d1fcbcf5cf4
neyaznafiz <niiyazobug@gmail.com>
2022-12-07 15:18:55 +0600

    brand and stock toggle UI  impliment in home component with activeClass

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
-import { toggleBrand, toggleStock } from "../../redux/actionCreators/filterActions";
+import {
+  toggleBrand,
+  toggleStock,
+} from "../../redux/actionCreators/filterActions";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
-  console.log(products);
-  const activeClass = "text-white bg-[#ccac00] border-white";
-
+  // console.log(products);
   const dispatch = useDispatch();
+  const filters = useSelector((state) => state.filter.filters);
+  const { brands, stock } = filters;
+  console.log(brands, stock);
+
+  const activeClass = "text-white bg-[#ccac00] border-white";
 
   useEffect(() => {
     fetch("http://localhost:5000/products")
@@ -21,19 +27,25 @@ const Home = () => {
       <div className="mb-10 flex justify-end gap-5">
         <button
           onClick={() => dispatch(toggleStock())}
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            stock ? activeClass : null
+          } `}
         >
           In Stock
         </button>
         <button
           onClick={() => dispatch(toggleBrand("amd"))}
-          className={`border px-3 py-2 rounded-full font-semibold`}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("amd") ? activeClass : null
+          }`}
         >
           AMD
         </button>
         <button
           onClick={() => dispatch(toggleBrand("intel"))}
-          className={`border px-3 py-2 rounded-full font-semibold`}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("intel") ? activeClass : null
+          }`}
         >
           Intel
         </button>
